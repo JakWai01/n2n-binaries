076b07d7f41b6fce88b08ff4efe18810c8ea1c7e
JakWai01 <jakob.waibel@web.de>
2021-03-09 09:34:20 +0100

    feat: Add sudo

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ curl -L -o /tmp/2.8.tar.gz https://github.com/ntop/n2n/archive/2.8.tar.gz
 cd /tmp
 tar -zxvf 2.8.tar.gz
 cd n2n-2.8
-dnf install @development-tools
-dnf install autoconf automake autogen libtool
+sudo dnf install @development-tools
+sudo dnf install autoconf automake autogen libtool
 ./autogen.sh
 ./configure
 make
